291c3122855faf84e40cdd1703669b095f8711fe
Lucin <lucin189@gmail.com>
2022-02-09 17:26:37 -0300

    :sparkles: Update submitButton EventListener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,39 @@
 const nameField = document.getElementById("inputName");
 const emailField = document.getElementById("inputEmail");
 const telephoneField = document.getElementById("inputTel");
-
 const submitButton = document.getElementById("submitBtn");
 
 const contactBook = [];
 
 function addNewEntry() {
-    const newEntry = [{
+    const newEntry = {
         name: nameField.value,
         email: emailField.value,
         telephone: telephoneField.value
-    }];
-
+    };
+    
     
     contactBook.push(newEntry);
     console.log(contactBook);
+    
+}
 
+// TODO: Fix bug adding doubled contacts on the page
+function contactLoop() {
+    const ul = document.getElementById("TARGET");
+    for (let key of contactBook) {
+        let newLi = document.createElement("li");
+        
+        newLi.innerHTML = `
+            <button class="removeContact"></button>
+            <h2>${key.name}</h2>
+            <p>${key.email}</p>
+            <span>${key.telephone}</span>
+        `;
+
+        ul.appendChild(newLi);
+    }
 }
 
 submitButton.addEventListener("click", addNewEntry);
+submitButton.addEventListener("click", contactLoop);
